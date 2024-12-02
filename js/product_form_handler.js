6f399526e3f37bc73fb030ebbf24fa62f2782e08
J. Paul Duplantis <paulduplantis@gmail.com>
2024-12-02 14:57:50 -0500

    Update product_form_handler.js

--- js/product_form_handler.js
+++ js/product_form_handler.js
@@ -34,13 +34,25 @@ document.getElementById('productForm').addEventListener('submit', function (e) {
 
     // Combine into List Item
     const htmlContent = `
-        <li>
-            <a href="${product.productBuyLink}" data-largesrc="${product.productImage || ''}" data-title="${product.productName}" data-description="${product.previewSubject || 'N/A'}">
-                ${tilePreview}
-            </a>
-            ${expanderContent}
-        </li>
-    `;
+     const htmlContent = `
+<li>
+    <a href="${product.productBuyLink}" 
+       data-largesrc="${product.productImage}" 
+       data-title="${product.productName}" 
+       data-description="${product.productDescription} | Price: ${product.productPrice} | RDF6: Subject - ${product.rdfSubject}, Predicate - ${product.rdfPredicate}, Object - ${product.rdfObject}, Type - ${product.rdfType}, Subtype - ${product.rdfSubtype}, Relationship - ${product.rdfRelationship}">
+        <div class="tile">
+            <div class="product-badge">${product.productName}</div>
+            <div class="text-group">
+                <div class="word subject">${product.rdfSubject}</div>
+                <div class="word predicate">${product.rdfPredicate}</div>
+                <div class="word object">${product.rdfObject}</div>
+                <div class="word type">${product.rdfType}</div>
+                <div class="word subtype">${product.rdfSubtype}</div>
+                <div class="word relationship">${product.rdfRelationship}</div>
+            </div>
+        </div>
+    </a>
+</li>`;
 
     // Display HTML
     document.getElementById('html-output').innerText = htmlContent.trim();
